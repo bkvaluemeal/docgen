023b36aefdf6a868c0d33b661eb5ae65e3f6fc49
Justin Willis <sirJustin.Willis@gmail.com>
2017-02-21 22:48:41 -0500

    Added entry point
    Created an entry point application to run Docgen from the command line

---
+++ docgen/__main__.py
@@ -0,0 +1,32 @@
+import argparse
+import docgen
+import os
+
+def main():
+	'''
+	The main method and entry point of the application
+	'''
+
+	parser = argparse.ArgumentParser(
+		description = 'docgen v%s' % docgen.__version__,
+		epilog = 'Copyright (c) 2017, Justin Willis',
+		prog = 'docgen',
+		formatter_class = argparse.RawTextHelpFormatter
+	)
+	parser.add_argument(
+		'-v', '--version',
+		action = 'version',
+		version = 'docgen v%s' % docgen.__version__
+	)
+	parser.add_argument(
+		'package',
+		help = 'the name of the package in the current directory',
+		default = os.path.relpath('.', '..'),
+		nargs = '?'
+	)
+	args = parser.parse_args()
+
+	print('Generating documentation for package %s' % args.package)
+
+if __name__ == '__main__':
+	main()
---
+++ setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+
+def format(input, start = 0):
+	result = ''
+	indent = False
+	count = 0
+
+	with open(input, 'r') as file:
+		for line in file:
+			if count > start:
+				if line[:1] == '\t' and not indent:
+					indent = True
+					result += '::\n\n'
+
+				if line[:1].isalnum() and indent:
+					indent = False
+
+				result += line.replace('> ', '\t').replace('>', '\t')
+			count += 1
+
+	return result
+
+blurb = ('Docgen is a Python module that generates markdown documentation for'
+ 	' Python code that adheres to Google\'s style guide.\n'
+)
+
+setup(
+	name = 'docgen',
+	version = '0.0.0',
+	author = 'Justin Willis',
+	author_email = 'sirJustin.Willis@gmail.com',
+	packages = find_packages(),
+	include_package_data = True,
+	zip_safe = False,
+	url = 'https://bitbucket.org/bkvaluemeal/docgen',
+	license = 'ISC License',
+	description = 'A markdown documentation generator for Python',
+	long_description = blurb + format('README.md', 3),
+	entry_points = {
+		'console_scripts': [
+			'docgen = docgen.__main__:main'
+		]
+	},
+	classifiers = [
+		'Development Status :: 1 - Planning',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: ISC License (ISCL)',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+		'Topic :: Documentation'
+	],
+	keywords = 'markdown documentation generator',
+	install_requires = [
+	]
+)
